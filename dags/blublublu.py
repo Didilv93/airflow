70f46c1d7b3cf6ea875a5ee36c459417a4ede57a
Diego Silva <diegossufmg@gmail.com>
2026-04-07 16:56:07 +0100

    Update environment variables and Docker Compose configuration for Airflow setup

--- dags/blublublu.py
+++ dags/blublublu.py
@@ -5,11 +5,11 @@ from datetime import datetime
 with DAG(
     dag_id="create_dummy_file",
     start_date=datetime(2024, 1, 1),
-    schedule_interval=None,
+    schedule=None,
     catchup=False,
     description="Cria um arquivo .dummy usando BashOperator",
 ) as dag:
     create_dummy = BashOperator(
         task_id="create_dummy_file",
-        bash_command="touch /opt/airflow/dags/.dummy"
+        bash_command="touch /opt/airflow/dags/.dummy",
     )
